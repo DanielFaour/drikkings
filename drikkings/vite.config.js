df566446c635b94eba63692d08105512fba35784
DanielFaour <daniea1602@gmail.com>
2025-03-27 03:58:38 +0100

    Revert "fixed 404???"
    This reverts commit de5660b57256fdf741b7c30bc29fe4527215ed48.

--- drikkings/vite.config.js
+++ drikkings/vite.config.js
@@ -3,15 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'public/index.html',
-          dest: '404.html', // Make a copy of index.html as 404.html
-        },
-      ],
-    }),
-  ],
-  base: '/',
+  plugins: [react()],
+  // base: '/drikkings/'
 })
