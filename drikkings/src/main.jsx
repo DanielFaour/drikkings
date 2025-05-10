167022d8ef9dadea4fa157730937f7089422c612
DanielFaour <daniea1602@gmail.com>
2025-05-10 23:36:02 +0200

    Update main.jsx

--- drikkings/src/main.jsx
+++ drikkings/src/main.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App.jsx';
+import ScrollToTop from './components/ScrollTop.js';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
+  <>
+    <BrowserRouter basename='/'>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
-  </React.StrictMode>
-)
+  </>
+);
