debf7149345b603aacb6d45b51d912c1986e6020
DanielFaour <daniea1602@gmail.com>
2025-05-14 04:01:18 +0200

    Update Game1.jsx

--- drikkings/src/components/games/Game1.jsx
+++ drikkings/src/components/games/Game1.jsx
@@ -27,13 +27,13 @@ function Game1() {
     clickDownSoundRef.current = new Howl({
       src: [clickDownSound], 
       rate: 1, 
-      volume: 0.4, 
+      volume: 0.5, 
       html5: false, 
       preload: true });
     clickUpSoundRef.current = new Howl({
       src: [clickUpSound], 
       rate: 1, 
-      volume: 0.4, 
+      volume: 0.5, 
       html5: false, 
       preload: true });
     explosionSoundRef.current = new Howl({
